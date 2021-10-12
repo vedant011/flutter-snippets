0b47a10bf17e40df6f7731bbeb1b7708b6fe6d7d
vedant011 <64039474+vedant011@users.noreply.github.com>
2021-10-12 10:45:35 -0700

    Add files via upload

---
+++ VideoPlayer2/widget/orientation/portrait_landscape_player_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:video_example/main.dart';
+import 'package:video_example/widget/orientation/video_player_both_widget.dart';
+import 'package:video_player/video_player.dart';
+
+class PortraitLandscapePlayerPage extends StatefulWidget {
+  @override
+  _PortraitLandscapePlayerPageState createState() =>
+      _PortraitLandscapePlayerPageState();
+}
+
+class _PortraitLandscapePlayerPageState
+    extends State<PortraitLandscapePlayerPage> {
+  VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = VideoPlayerController.network(urlLandscapeVideo)
+      ..addListener(() => setState(() {}))
+      ..setLooping(true)
+      ..initialize().then((_) => controller.play());
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(toolbarHeight: 0),
+        body: VideoPlayerBothWidget(controller: controller),
+      );
+}
